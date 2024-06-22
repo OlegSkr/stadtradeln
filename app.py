505c772742370e9961778929798910a588172fdf
Oleg Skrypnyuk <osk@norcom.de>
2024-06-22 21:32:43 +0200

    saving html contents for debugging

--- app.py
+++ app.py
@@ -59,6 +59,27 @@ try:
 except Exception as error:
     print("Exception 3:", error)
 
+try:
+
+    contents = 'hello world'
+    
+    with open('/home/osk/stadtradeln/contents.html') as f:
+        contents = " ".join([l.rstrip("\n") for l in f])
+
+    print('contents received, length: ')
+    print(len(contents))
+    print
+    result = re.search(r"https:\/\/api.stadtradeln.de\/v1\/kmbook\/(\b\d+)\/add", contents)
+    print('re.search done, groups:')
+    print(result.groups())
+    print
+    print('group 1:')
+    print(result.group(1))
+    print        
+except Exception as error:
+    print("Exception 5:", error)
+
+
 print
 print
 print
@@ -101,10 +122,14 @@ def get_stadtradeln_contents() -> dict:
     print(response.text)
 
     response.raise_for_status()
-
+    
     response:str = session.get(stadtradeln_url)
-
-    return response.text
+    
+    contents = response.text.copy()
+    with open("/workspace/contents.html", "w") as file:
+        file.write(contents)
+        
+    return contents
 
 
 @app.route('/')
